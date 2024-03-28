48636d01bcb3892a69e3c75eca4cd1ce7484eb88
n-alonso <nmalonsojuan@gmail.com>
2024-03-28 13:16:22 +0200

    init

---
+++ config/env/production/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      breakpoints: {
+        xxlarge: 1920,
+        xlarge: 1440,
+        large: 1280,
+        medium: 1024,
+        small: 768,
+        xsmall: 376,
+      },
+    },
+  },
+});
